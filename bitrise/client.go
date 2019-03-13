d31405f68a95f758342831f545fa4c0cdc70c8d9
Takuma Homma <nagomimatcha@gmail.com>
2019-03-13 22:53:03 +0900

    Add func whether response is successful or not

--- bitrise/client.go
+++ bitrise/client.go
@@ -8,3 +8,10 @@ import (
 const (
 	bitriseApiEndpoint = "https://app.bitrise.io/app/"
 )
+
+func checkResponse(response *http.Response, err error) (*http.Response, error) {
+	switch response.StatusCode {
+	case 200, 201:
+		return response, nil
+	}
+}

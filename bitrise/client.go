3743310e89138a2908d1a409e0cb48cf779f1de0
Takuma Homma <nagomimatcha@gmail.com>
2019-03-14 17:06:00 +0900

    Handle error response

--- bitrise/client.go
+++ bitrise/client.go
@@ -1,9 +1,16 @@
 package bitrise
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
+
+type ErrorMsg struct {
+	Message string `json:"error_msg"`
+}
 
 const (
 	bitriseApiEndpoint = "https://app.bitrise.io/app/"
@@ -13,5 +20,16 @@ func checkResponse(response *http.Response, err error) (*http.Response, error) {
 	switch response.StatusCode {
 	case 200, 201:
 		return response, nil
+	default:
+		var reader io.Reader = response.Body
+		reader = io.TeeReader(r, os.Stderr)
+
+		var errorMsg ErrorMsg
+		err = json.NewDecoder(reader).Decode(&errorMsg)
+		if err != nil {
+			return nil, fmt.Errorf(err)
+		} else {
+			return nil, fmt.Errorf("Status code: %s", response.Status)
+		}
 	}
 }
